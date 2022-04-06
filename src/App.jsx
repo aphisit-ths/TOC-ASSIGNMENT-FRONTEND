0a41aec249652e995a029358c72737ff011291af
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-04-06 21:18:26 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import GeoChart from "./GeoChart";
+import data from "./GeoChart.world.geo.json";
+import "./App.scss";
+import Intro from "./components/intro"
+function App() {
+  const [property, setProperty] = useState("formal_en");
+  return (
+    <div className="root">
+      <Intro></Intro>
+      <div className="box">
+        <GeoChart data={data} property={property} />
+      </div>
+    </div >
+  );
+}
+
+export default App;
+
+
+// Editing by Aphisit Thubsaeng
---
+++ src/components/intro/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./intro.scss";
+function Intro() {
+  return (
+    <div className="intro">
+      <h1>อยากรู้หรือไม่ว่า</h1>
+    </div>
+  );
+}
+
+export default Intro;
