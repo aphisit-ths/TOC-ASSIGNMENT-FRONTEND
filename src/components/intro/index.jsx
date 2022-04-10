5d463a2830d6118d1870f5a41c00b43f708dc4e5
WaveCE29 <sarin120@hotmail.com>
2022-04-10 23:17:40 +0700

    add intro

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -3,7 +3,10 @@ import "./intro.scss";
 function Intro() {
   return (
     <div className="intro">
-      <h1>อยากรู้หรือไม่ว่า</h1>
+      <p className="img"><img src="https://www.maropeng.co.za/uploads/imager/files/19832/world-1303628_1920_399f7ced9821ef832957bb0ed8fd3ff5.png" alt="center" /></p>
+      <p><h1>ข้อมูลประเทศและผู้นำโลก</h1></p>
+      <p><h2>เพื่อนๆ อยากรู้หรือไม่ว่าในแต่ละประเทศมีประชากรเท่าไหร่ และตอนนี้มีใครเป็นผู้นำอยู่บ้าง</h2></p>
+      <p><button class="button button2">ค้นหาเลย !</button></p>
     </div>
   );
 }
