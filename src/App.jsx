e2008d3b3c105a344bf4b41e991a535f36e08062
WaveCE29 <sarin120@hotmail.com>
2022-04-11 16:35:39 +0700

    fix button in intro

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import GeoChart from "./components/geoChart/GeoChart";
 import data from "./components/geoChart/GeoChart.world.geo.json";
 import "./App.scss";
 import Intro from "./components/intro";
-import Button1 from "./components/buttonClick/button";
+const data1 = {
+  "data": [
+    {
+      "name": "Afghanistan",
+      "Head Of Government": "Prime Minister: Kishida Fumio"
+    },
+    {
+      "name": "Thailand",
+      "Head Of Government": "Prime Minister: Kishida Fumio"
+    },
+    { 
+      "name": "Japan", 
+      "Head Of Government": "Prime Minister: Kishida Fumio" }
+  ]
+}
+
 function App() {
   const [property, setProperty] = useState("formal_en");
   return (
     <div className="root">
-      <Intro></Intro>
-     <Button1></Button1>
-      <div className="box">
+      <Intro data={data1}></Intro>
+      <div className="box" id="button12">
       <input type="text" placeholder="ค้นหาชื่อประเทศเช่น ไทย , ญี่ปุ่น "  />
        <GeoChart data={data} property={property} /> 
       </div>
--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./intro.scss";
-function Intro() {
+function Intro({data}) {
+  console.log(data)
   return (
     <div className="intro">
       <p className="img"><img src="https://www.maropeng.co.za/uploads/imager/files/19832/world-1303628_1920_399f7ced9821ef832957bb0ed8fd3ff5.png" alt="center" /></p>
       <p><h1>ข้อมูลประเทศและผู้นำโลก</h1></p>
       <p><h2>เพื่อนๆ อยากรู้หรือไม่ว่าในแต่ละประเทศมีประชากรเท่าไหร่ และตอนนี้มีใครเป็นผู้นำอยู่บ้าง</h2></p>
+      <a href="#button12" className="button button2" >Click me</a>
+       
     </div>
   );
 }
